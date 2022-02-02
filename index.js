128fac4129252421e6d44419a0c1647b48a4c082
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2022-02-02 09:34:42 +0900

    MongoDB Install , User ( model , Schema ) 작성

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://youngsik:a1234@boilerplate.g5nd9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
 app.get('/', (req, res) => res.send('Hello World'));
+
 app.listen(port, () => console.log(`Example app listening on port $(port}!`));
